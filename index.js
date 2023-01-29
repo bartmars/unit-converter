2e909a6350caa8056268f0fb9377e134e8c46fb7
Bart Mars <32029310+bartmars@users.noreply.github.com>
2023-01-29 14:39:56 +0100

    update project

--- index.js
+++ index.js
@@ -10,22 +10,28 @@ let lengthP = document.getElementById("length-p")
 let volumeP = document.getElementById("volume-p")
 let massP = document.getElementById("mass-p")
 
-baseStringLength = `0 meters = 0.000 feet | 0 feet = 0.000 meters`
-baseStringVolume = `0 liters = 0.000 gallons | 0 gallons = 0.000 liters`
-baseStringMass = `0 kilos = 0.000 pounds | 0 pounds = 0.000 kilos`
+
+let number = convertIp.value
 
 convertBtn.addEventListener("click", function() {
-    convertLengthToFeet()
-    convertLengthToMeter()
-    convertVolumeToGallons()
-    convertVolumeToLiters()
-    convertMassToPounds()
-    convertMassToKilograms()
+    console.log("clicky-the-click")
+
+    let lengthToFeet = multiply(number, 3.281)
+    let lengthToMeter = divide(number, 3.281)    
+    let volumeToGallons = multiply(number, 0.264)
+    let volumeToLiters = divide(number, 0.264)
+    let massToPounds = multiply(number, 2.204)
+    let massToKilograms = divide(number, 2.204)
+
+    lengthP.textContent = `${number} meters = ${lengthToFeet.toFixed(3)} feet | ${number} feet = ${lengthToMeter.toFixed(3)} meters`
+    volumeP.textContent = `${number} liters = ${volumeToGallons.toFixed(3)} gallons | ${number} gallons = ${volumeToLiters.toFixed(3)} liters`
+    massP.textContent = `${number} kilos = ${massToPounds.toFixed(3)} pounds | ${number} pounds = ${massToKilograms.toFixed(3)} kilos`
 })
 
-function convertLengthToFeet() {}
-function convertLengthToMeter() {}
-function convertVolumeToGallons() {}
-function convertVolumeToLiters() {}
-function convertMassToPounds() {}
-function convertMassToKilograms() {}
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
