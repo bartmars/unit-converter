c35636b09a9618f1b1f8883f78cdd635b3bff2bf
Bart Mars <32029310+bartmars@users.noreply.github.com>
2023-01-29 08:57:06 +0100

    update project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # unit-converter
 Scrimba Soloproject from the Frontend Developer Career Path
+
+Unit converter
+
+Length / Volume / Mass
+
+Goals
+* Follow the design spec (light or dark)
+* Generate all conversions when the user clicks “Convert”
+* Round the number down to three decimal places
--- index.js
+++ index.js
@@ -3,3 +3,29 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const convertIp = document.getElementById("convert-ip")
+const convertBtn = document.getElementById("convert-btn")
+let lengthP = document.getElementById("length-p")
+let volumeP = document.getElementById("volume-p")
+let massP = document.getElementById("mass-p")
+
+baseStringLength = `0 meters = 0.000 feet | 0 feet = 0.000 meters`
+baseStringVolume = `0 liters = 0.000 gallons | 0 gallons = 0.000 liters`
+baseStringMass = `0 kilos = 0.000 pounds | 0 pounds = 0.000 kilos`
+
+convertBtn.addEventListener("click", function() {
+    convertLengthToFeet()
+    convertLengthToMeter()
+    convertVolumeToGallons()
+    convertVolumeToLiters()
+    convertMassToPounds()
+    convertMassToKilograms()
+})
+
+function convertLengthToFeet() {}
+function convertLengthToMeter() {}
+function convertVolumeToGallons() {}
+function convertVolumeToLiters() {}
+function convertMassToPounds() {}
+function convertMassToKilograms() {}
